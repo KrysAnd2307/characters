089a331809934387c8c1867528feaf141f458a8d
Krystian <250939@edu.p.lodz.pl>
2024-05-09 17:47:40 +0200

    menu and do while

--- main.cpp
+++ main.cpp
@@ -2,7 +2,35 @@
 #include <stdlib.h>
 #include "array.h"
 
+void menu(){
+    printf("1 - Enter Text\n");
+    printf("2 - Display Text\n");
+    printf("3 - Count the characters\n");
+    printf("4 - End the program\n");
+}
+
+
+
 int main() {
     char text[SIZE] = "";
+    int choice = 0;
+
+    do{
+        menu();
+        scanf("%d", &choice);
+        switch(choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+
+    }while(choice != 4);
+
+
     return 0;
 }
